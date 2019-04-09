813d35e8d6e5647f6bb21f881fd7c9980c1835fd
Andrew McIntosh <nacho.vonkickbutt@gmail.com>
2019-04-09 13:20:36 -0400

    Basic response

--- webhook_tester/__init__.py
+++ webhook_tester/__init__.py
@@ -1,12 +1,35 @@
-from flask import Flask
+import logging
+from flask import Flask, request
+
+log = logging.getLogger(__name__)
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
 
-    @app.route('/listen')
+    @app.route('/')
     def hello():
-        return 'Received'
+        return 'Running'
+
+    @app.route('/listen', methods=['GET', 'POST'])
+    def listen():
+        resp = 'Received: {}. -- '.format(request.full_path)
+        log.info(resp)
+        log.info('parameters: ')
+        for key in request.form:
+            arg = '{}:{} '.format(key, request.form[key])
+            log.info(arg)
+            resp += arg
+
+        log.info('headers: ')
+        for arg in request.headers:
+            log.info(arg)
+
+        return resp
+
+    @app.route('/listen_fail', methods=['GET', 'POST'])
+    def listen_fail():
+        return listen(), 404
 
     return app
